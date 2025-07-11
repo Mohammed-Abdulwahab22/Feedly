1470131b4e2b465d04b9cb1b0046bfb11e4f43fb
Mohammed Abdulwahab <MohammedAbdulwahab22_@outlook.com>
2025-07-11 10:33:36 +0300

    creating private route component and using it to make sure the user is authenticated.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,24 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import {Home} from "./Pages/Home";
-import {FeedbackDetails} from "./Pages/FeedbackDetails";
-import {Login} from "./Pages/Login";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { Home } from "./Pages/Home";
+import { FeedbackDetails } from "./Pages/FeedbackDetails";
+import { Login } from "./Pages/Login";
+import { Register } from "./Pages/Register";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/feedback/:id" element={<FeedbackDetails />} />
+        <Route path="/register" element={<Register />} />
+
+        <Route element={<PrivateRoute />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/feedback/:id" element={<FeedbackDetails />} />
+        </Route>
+
+
+        <Route path="*" element={<Navigate to="/login" />} />
       </Routes>
     </Router>
   );
