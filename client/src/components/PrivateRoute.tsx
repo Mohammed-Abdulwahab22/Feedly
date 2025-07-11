5adda3a3800d117cc823c887cc7b03ae50f2d0d8
Mohammed Abdulwahab <MohammedAbdulwahab22_@outlook.com>
2025-07-11 11:24:31 +0300

    using storage helper functions in privateRoute component.

--- client/src/components/PrivateRoute.tsx
+++ client/src/components/PrivateRoute.tsx
@@ -1,9 +1,25 @@
+import React from "react";
 import { Navigate, Outlet } from "react-router-dom";
+import { getItem } from "../utils/storage"; 
 
 const PrivateRoute = () => {
-  const token = localStorage.getItem("token");
 
-  return token ? <Outlet /> : <Navigate to="/login" />;
+  const [isAuthenticated, setIsAuthenticated] = React.useState<boolean | null>(null);
+
+  React.useEffect(() => {
+    const checkToken = async () => {
+      const token = await getItem<string>("token"); // Specify the expected type of the token
+      setIsAuthenticated(!!token); // Convert token (string | null) to boolean
+    };
+
+    checkToken();
+  }, []); 
+
+  if (isAuthenticated === null) {
+    return <div>Loading...</div>; 
+  }
+
+  return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
 };
 
 export default PrivateRoute;
