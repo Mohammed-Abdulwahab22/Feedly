4666384d2714e041c4d416638112109880429205
Mohammed Abdulwahab <MohammedAbdulwahab22_@outlook.com>
2025-07-13 11:07:16 +0300

    saving user id in local storage.

--- client/src/Pages/Login.tsx
+++ client/src/Pages/Login.tsx
@@ -30,7 +30,7 @@ export const Login = () => {
     const response = await LoginUser(emailOrUsername, password);
     if (response.token) {
       setItem("token", response.token);
-      setItem("userId", JSON.stringify(response.userId));
+      setItem("userId", response.userId);
       toast.success("Login successful");
       navigate("/");
     } else {
