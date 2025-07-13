4905b417fddb86f4c080e4eef49a632d524d409f
Mohammed Abdulwahab <MohammedAbdulwahab22_@outlook.com>
2025-07-13 11:13:23 +0300

    modifying create comment function.

--- client/src/api/comments.ts
+++ client/src/api/comments.ts
@@ -2,15 +2,24 @@ import axios from 'axios';
 
 const API_BASE = "http://localhost:3000/api";
 
-export async function createComment(postId: string, content: string, userId: string) {
-  try {
-    const response = await axios.post(`${API_BASE}/comments`, {
-      content,
-      postId,
-      userId
-    });
-    return response.data;
-  } catch (error: any) {
-    throw new Error(error?.response?.data?.error || "Failed to create comment");
-  }
+export async function addComment(postId: string, content: string, userId: string) {
+    const token = localStorage.getItem("token");
+    if (!token) {
+        throw new Error("User not authenticated");
+    }
+    
+    try {
+        const response = await axios.post(`${API_BASE}/comments`, {
+            content,
+            postId,
+            userId
+        },{
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        });
+        return response.data;
+    } catch (error: any) {
+        throw new Error(error?.response?.data?.error || "Failed to create comment");
+    }
 }
